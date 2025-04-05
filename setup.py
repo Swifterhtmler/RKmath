b358279c5f745c604bde6fd967b549be5a297fb6
Riku Kuisma <130903460+Swifterhtmler@users.noreply.github.com>
2025-04-05 15:21:56 +0300

    first commit

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='rkmath',
-    packages=find_packages(include=['rmath']),
-    version='0.2.2',
+    packages=find_packages(include=['rkmath']),
+    version='0.2.5',
     description='Math library for easy math operations',
      long_description=long_description,
     long_description_content_type="text/markdown",
     author='Swifterhtmler',
-    install_requires=["numpy==2.20.0"],
+    install_requires=["numpy>=1.21.0"],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
